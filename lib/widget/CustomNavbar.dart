5979d26fa01d96737c4f983e31ab5e9a79b248e1
aurora <aurora.nardelli99@gmail.com>
2024-01-08 22:22:02 +0100

    minuto 7:44

---
+++ lib/widget/CustomNavbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//7:44
+
+class CustomNavbar extends StatefulWidget {
+  @override
+  State<CustomNavbar> createState() => _CustomNavbarState();
+}
+
+class _CustomNavbarState extends State<CustomNavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
