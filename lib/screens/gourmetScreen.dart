86159d01605516b509a8abac850cce8941697f0c
elisa <so.elisa9@gmail.com>
2024-01-15 17:34:44 +0100

    creazione pagine categorie fusionScreen.dart gourmetScreen.dart

---
+++ lib/screens/gourmetScreen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:vogliadifood_app/screens/CategorieScreen.dart';
+import 'package:vogliadifood_app/screens/homeScreen.dart';
+import 'package:vogliadifood_app/utils/colors.dart';
+import 'package:vogliadifood_app/widget/ListaRistorantiCategorie.dart';
+
+import '../utils/helper.dart';
+import '../widget/CustomNavbar.dart';
+
+class gourmetScreen extends StatelessWidget {
+  static const routeName = "/gourmetScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                height: Helper.getScreenHeight(context),
+                width: Helper.getScreenWidth(context),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: Image.asset(
+                        Helper.getAssetName("Sfondo_app2.png", "virtual"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                              ),
+                              child: Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).pushReplacementNamed(CategorieScreen.routeName);
+                                    },
+                                    child: Icon(
+                                      Icons.arrow_back_ios_rounded,
+                                      color: AppColors.Bianco,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      "Categorie",
+                                      style: Helper.getTheme(context).headline6,
+                                    ),
+                                  ),
+                                  Image.asset(
+                                    Helper.getAssetName(
+                                        "shopping_cart.png", "virtual"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 250,
+                            child: Image.asset(
+                              Helper.getAssetName(
+                                  "vogliadigourmet.jpg", "virtual"),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Text(
+                              "La voglia di un pasto con ingrendienti di alta qualità e grande cura nella realizzazione.",
+                              style: Helper.getTheme(context).headline5,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 300,
+                            padding: const EdgeInsets.only(left: 20),
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: [
+                                  ListaRistorantiCategorie(
+                                    image: Image.asset(
+                                      Helper.getAssetName(
+                                          "LaRossa.png", "virtual"),
+                                      fit: BoxFit.cover,
+                                    ),
+                                    name: "Ristorante Larossa",
+                                    categoria: "Fast Food",
+                                    rate: '5',
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  ListaRistorantiCategorie(
+                                    image: Image.asset(
+                                      Helper.getAssetName(
+                                          "gourmet_pesce.png", "virtual"),
+                                      fit: BoxFit.cover,
+                                    ),
+                                    name: "Baleno - Idea di mare",
+                                    categoria: "Fast Food",
+                                    rate: '4.5',
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: CustomNavbar(
+              categorie: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
